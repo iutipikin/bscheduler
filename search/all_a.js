52120bd7f98074ec8211e02e961c797ca6881015
Travis CI <travis@travis-ci.org>
2017-08-30 16:01:00 +0000

    Deploy to gh-pages.

--- search/all_a.js
+++ search/all_a.js
@@ -4,5 +4,6 @@ var searchData=
   ['lbuffer',['LBuffer',['../structfactory_1_1LBuffer.html',1,'factory']]],
   ['lbuffer_3c_20char_20_3e',['LBuffer&lt; char &gt;',['../structfactory_1_1LBuffer.html',1,'factory']]],
   ['lbuffertest',['LBufferTest',['../structLBufferTest.html',1,'']]],
+  ['local_5fserver',['local_server',['../classfactory_1_1local__server.html',1,'factory']]],
   ['location',['Location',['../structdiscovery_1_1Location.html',1,'discovery']]]
 ];
