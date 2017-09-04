0fde7b9a3a8323999273ba03f58dcdece6a10442
Travis CI <travis@travis-ci.org>
2017-09-04 23:32:11 +0000

    Deploy to gh-pages.

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['acf_5fgenerator',['ACF_generator',['../structautoreg_1_1ACF__generator.html',1,'autoreg']]],
-  ['application',['Application',['../structfactory_1_1Application.html',1,'factory']]],
+  ['application',['Application',['../classfactory_1_1Application.html',1,'factory']]],
   ['array',['Array',['../structautoreg_1_1Array.html',1,'autoreg']]],
   ['array_3c_20t_2c_201_20_3e',['Array&lt; T, 1 &gt;',['../structautoreg_1_1Array_3_01T_00_011_01_4.html',1,'autoreg']]],
   ['autoreg_5fapp',['Autoreg_app',['../structAutoreg__app.html',1,'']]],
