d5abafa4abf899a9b0114194eb50a05c09878a91
Travis CI <travis@travis-ci.org>
2017-09-13 14:22:03 +0000

    Deploy to gh-pages.

--- search/classes_10.js
+++ search/classes_10.js
@@ -9,6 +9,7 @@ var searchData=
   ['slice',['Slice',['../structSlice.html',1,'']]],
   ['smallsizetest',['SmallSizeTest',['../structSmallSizeTest.html',1,'']]],
   ['socket_5fpipeline',['socket_pipeline',['../classfactory_1_1socket__pipeline.html',1,'factory']]],
+  ['socket_5fpipeline_5fkernel',['socket_pipeline_kernel',['../classfactory_1_1socket__pipeline__kernel.html',1,'factory']]],
   ['solve_5fyule_5fwalker',['Solve_Yule_Walker',['../structautoreg_1_1Solve__Yule__Walker.html',1,'autoreg']]],
   ['spec_5fapp',['Spec_app',['../structSpec__app.html',1,'']]],
   ['spectrum_5fapp',['Spectrum_app',['../structSpectrum__app.html',1,'']]],
