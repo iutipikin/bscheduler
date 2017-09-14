33d2657847abb28db480ee0559f72df8c8c23703
Travis CI <travis@travis-ci.org>
2017-09-14 17:54:12 +0000

    Deploy to gh-pages.

--- search/classes_1.js
+++ search/classes_1.js
@@ -11,6 +11,5 @@ var searchData=
   ['basic_5fsocket_5fpipeline_3c_20t_2c_20process_5fhandler_3c_20t_2c_20basic_5frouter_3c_20t_20_3e_20_3e_20_3e',['basic_socket_pipeline&lt; T, process_handler&lt; T, basic_router&lt; T &gt; &gt; &gt;',['../structfactory_1_1basic__socket__pipeline.html',1,'factory']]],
   ['basic_5fsocket_5fpipeline_3c_20t_2c_20process_5fhandler_3c_20t_2c_20router_20_3e_20_3e',['basic_socket_pipeline&lt; T, process_handler&lt; T, Router &gt; &gt;',['../structfactory_1_1basic__socket__pipeline.html',1,'factory']]],
   ['basic_5fsocket_5fpipeline_3c_20t_2c_20remote_5fclient_3c_20t_2c_20socket_2c_20router_20_3e_20_3e',['basic_socket_pipeline&lt; T, remote_client&lt; T, Socket, Router &gt; &gt;',['../structfactory_1_1basic__socket__pipeline.html',1,'factory']]],
-  ['big_5fkernel',['Big_kernel',['../structBig__kernel.html',1,'']]],
-  ['bigsizetest',['BigSizeTest',['../structBigSizeTest.html',1,'']]]
+  ['big_5fkernel',['Big_kernel',['../structBig__kernel.html',1,'']]]
 ];
--- search/classes_10.js
+++ search/classes_10.js
@@ -5,7 +5,6 @@ var searchData=
   ['skew_5fnormal',['Skew_normal',['../classautoreg_1_1Skew__normal.html',1,'autoreg']]],
   ['sleepy_5fkernel',['Sleepy_kernel',['../structSleepy__kernel.html',1,'']]],
   ['slice',['Slice',['../structSlice.html',1,'']]],
-  ['smallsizetest',['SmallSizeTest',['../structSmallSizeTest.html',1,'']]],
   ['socket_5fpipeline',['socket_pipeline',['../classfactory_1_1socket__pipeline.html',1,'factory']]],
   ['socket_5fpipeline_5fkernel',['socket_pipeline_kernel',['../classfactory_1_1socket__pipeline__kernel.html',1,'factory']]],
   ['solve_5fyule_5fwalker',['Solve_Yule_Walker',['../structautoreg_1_1Solve__Yule__Walker.html',1,'autoreg']]],
