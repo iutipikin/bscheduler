6c49a4e4b06d8ea3cb9bbeda16c0b05c7611789a
Travis CI <travis@travis-ci.org>
2017-10-14 15:45:53 +0000

    Deploy to gh-pages.

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['remove',['remove',['../classbsc_1_1basic__handler.html#ab45e947bd08cab59bf2520448d34be76',1,'bsc::basic_handler::remove()'],['../classbsc_1_1process__handler.html#a081215300f43d2b3457daef4ffc2f4e4',1,'bsc::process_handler::remove()'],['../classbsc_1_1local__server.html#aae23464b68aa0f9799d6521a02226b41',1,'bsc::local_server::remove()'],['../classbsc_1_1remote__client.html#a393ec04d6672320ca61460dd4e47460e',1,'bsc::remote_client::remove()']]],
+  ['remove',['remove',['../classbsc_1_1basic__handler.html#ac0810efc624602608c1068634a739fe0',1,'bsc::basic_handler::remove()'],['../classbsc_1_1process__handler.html#ad533d90296edad101f48a184fbcd7404',1,'bsc::process_handler::remove()'],['../classbsc_1_1local__server.html#ac58f129ef8630643b8f5aef41b8db4d1',1,'bsc::local_server::remove()'],['../classbsc_1_1remote__client.html#aaefafee28cac6564c7f84d7805a45c89',1,'bsc::remote_client::remove()']]],
   ['run',['run',['../classbsc_1_1basic__socket__pipeline.html#a95f82a87a8b39d8f62f8ba3f7631d28a',1,'bsc::basic_socket_pipeline']]]
 ];
