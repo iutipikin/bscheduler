9a070350d2d18ba67c91c74d4298813d1096636b
Travis CI <travis@travis-ci.org>
2017-10-20 17:48:41 +0000

    Deploy to gh-pages.

--- search/classes_5.js
+++ search/classes_5.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['factory',['Factory',['../classbsc_1_1Factory.html',1,'bsc']]],
   ['factory_5fguard',['factory_guard',['../structbsc_1_1factory__guard.html',1,'bsc']]],
+  ['foreign_5fkernel',['foreign_kernel',['../classbsc_1_1foreign__kernel.html',1,'bsc']]],
   ['forward_5fto_5fchild',['forward_to_child',['../structbsc_1_1bits_1_1forward__to__child.html',1,'bsc::bits']]],
   ['forward_5fto_5fchild_3c_20r_20_3e',['forward_to_child&lt; R &gt;',['../structbsc_1_1bits_1_1forward__to__child.html',1,'bsc::bits']]],
   ['forward_5fto_5fparent',['forward_to_parent',['../structbsc_1_1bits_1_1forward__to__parent.html',1,'bsc::bits']]],
--- search/classes_6.js
+++ search/classes_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['generator1',['Generator1',['../structautoreg_1_1Generator1.html',1,'autoreg']]],
-  ['good_5fkernel',['Good_kernel',['../structGood__kernel.html',1,'']]]
+  ['good_5fkernel',['Good_kernel',['../structGood__kernel.html',1,'']]],
+  ['grand_5fmaster_5fkernel',['grand_master_kernel',['../classgrand__master__kernel.html',1,'']]]
 ];
